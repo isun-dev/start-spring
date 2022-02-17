5df427d69ee323d0320df2717858597e4de0ddd4
isun-dev <florachoi1124@naver.com>
2022-02-18 00:06:13 +0900

    feat: 전체 회원 조회 및 고유번호로 회원 조회하기 기능 추가

--- src/main/java/start/startspring/service/MemberService.java
+++ src/main/java/start/startspring/service/MemberService.java
@@ -4,6 +4,7 @@ import start.startspring.domain.Member;
 import start.startspring.repository.MemberRepository;
 import start.startspring.repository.MemoryMemberRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public class MemberService {
@@ -24,5 +25,17 @@ public class MemberService {
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
