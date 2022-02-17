9894a20bd1ab1f57eb540646546453a0f60de240
isun-dev <florachoi1124@naver.com>
2022-02-17 22:40:38 +0900

    feat: 회원 가입 함수 생성

--- src/main/java/start/startspring/service/MemberService.java
+++ src/main/java/start/startspring/service/MemberService.java
@@ -1,8 +1,17 @@
 package start.startspring.service;
 
+import start.startspring.domain.Member;
 import start.startspring.repository.MemberRepository;
 import start.startspring.repository.MemoryMemberRepository;
 
 public class MemberService {
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        memberRepository.save(member);
+        return member.getId();
+    }
 }
