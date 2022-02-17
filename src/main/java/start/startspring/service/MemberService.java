cbdd30c91ae9cadcfdf0a1889532b4ab38a38b24
isun-dev <florachoi1124@naver.com>
2022-02-17 23:44:36 +0900

    feat: 중복회원 예외 처리 추가 코드를 리팩토링하여 함수로 생성.

--- src/main/java/start/startspring/service/MemberService.java
+++ src/main/java/start/startspring/service/MemberService.java
@@ -14,11 +14,15 @@ public class MemberService {
      */
     public Long join(Member member) {
         // 중복회원 제거
+        validateDuplicate(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicate(Member member) {
         memberRepository.findByName(member.getName())
                 .ifPresent(m -> {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
-        memberRepository.save(member);
-        return member.getId();
     }
 }
